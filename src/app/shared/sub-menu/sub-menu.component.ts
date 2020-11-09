97dbdf5934f8642d8aa2c2be644bd58133b8f06d
Guerschom Tambwe <guerschomguerschom@gmail.com>
2020-11-09 14:43:34 +0200

    Getting rid of unnecessary files

--- src/app/shared/sub-menu/sub-menu.component.ts
+++ src/app/shared/sub-menu/sub-menu.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { User } from '@app/_models';
-import { AccountService, AuthenticationService } from '@app/_services';
+import { AccountService } from '@app/_services';
 
 @Component({
   selector: 'app-sub-menu',
